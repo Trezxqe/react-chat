b1c40eacab13dcc23a5b4c032b1a66ceb0b2eb95
Dmitry Rum <acopalypse@yandex.ru>
2021-09-30 11:50:00 +0300

    add default roomForm styles

--- frontend/src/components/createRoomForm/createRoomForm.jsx
+++ frontend/src/components/createRoomForm/createRoomForm.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import webSocket from '../../helpers/webSocket.jsx';
+import s from './createRoomForm.module.css';
 
 const CreateRoomForm = () => {
   const { username } = useSelector((state) => state);
@@ -16,16 +17,18 @@ const CreateRoomForm = () => {
   return (
     <div>
       <h3>Create Room</h3>
-      <form onSubmit={submitHandler}>
+      <form className={s.form} onSubmit={submitHandler}>
         <label>
           <span>Room name:</span>
-          <input type='text' name='roomName' required />
+          <input className={s.input} type='text' name='roomName' required />
         </label>
         <label>
           <span>Private:</span>
           <input type='checkbox' name='roomType' />
         </label>
-        <button type='submit'>Create</button>
+        <button className={s.button} type='submit'>
+          Create
+        </button>
       </form>
     </div>
   );
